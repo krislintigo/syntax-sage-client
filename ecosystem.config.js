d4d8b8f5bf92f63d2d45282173c950484e4b5043
1sses <vladzholon@gmail.com>
2023-12-29 18:06:48 +0300

    working

---
+++ ecosystem.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  apps: [
+    {
+      name: 'SS-C',
+      port: '4001',
+      exec_mode: 'cluster',
+      instances: 'max',
+      script: './.output/server/index.mjs',
+    },
+  ],
+}
