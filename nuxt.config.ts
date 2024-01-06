976166c97efc156fe15b05cfc437068e7f2d4101
1sses <vladzholon@gmail.com>
2024-01-06 15:02:45 +0300

    working

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -68,8 +68,8 @@ export default defineNuxtConfig({
     manifest: {
       name: 'Syntax Sage',
       short_name: 'Syntax Sage',
-      theme_color: '#141414',
-      background_color: '#141414',
+      theme_color: '#0a0a0a',
+      background_color: '#0a0a0a',
     },
   },
 })
