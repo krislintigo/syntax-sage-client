5fbae2ce869cbf4863ffc49a29a2d108f6ac9e5a
1sses <vladzholon@gmail.com>
2024-01-09 21:32:05 +0300

    working

--- utils/createProgressStatistics.ts
+++ utils/createProgressStatistics.ts
@@ -1,37 +1,38 @@
+const getTermStatus = (term: Term): keyof typeof termStatistics => {
+  const entries = Object.entries(term.studies)
+  const notStudiedFlag = entries.every(([, value]) => value === 0)
+  if (notStudiedFlag) return 'notStudied'
+  const learningFlag =
+    entries.some(([, value]) => value > 0) &&
+    entries.some(([, value]) => value < 3)
+  if (learningFlag) return 'learning'
+  const masteredFlag = entries.every(([, value]) => value >= 3)
+  if (masteredFlag) return 'mastered'
+  throw new Error('Something went wrong')
+}
+
 export const createProgressStatistics = (terms: Term[]) => {
   const statistics = _merge(_cloneDeep(termStatistics), {
     notStudied: { count: 0, percentage: 0 },
     learning: { count: 0, percentage: 0 },
     mastered: { count: 0, percentage: 0 },
   })
-  const notStudiedTerms = terms.reduce((acc, term) => {
-    const entries = Object.entries(term.studies)
-    const flag = entries.some(([, value]) => value === 0)
-    return flag ? acc + 1 : acc
-  }, 0)
-  const learningTerms = terms.reduce((acc, term) => {
-    const entries = Object.entries(term.studies)
-    const subFlag = entries.every(([, value]) => value !== 0)
-    const mainFlag = entries.some(([, value]) => value > 0 && value < 3)
-    return mainFlag && subFlag ? acc + 1 : acc
-  }, 0)
-  const masteredTerms = terms.reduce((acc, term) => {
-    const entries = Object.entries(term.studies)
-    const flag = entries.every(([, value]) => value >= 3)
-    return flag ? acc + 1 : acc
-  }, 0)
 
-  statistics.notStudied.count = notStudiedTerms
-  statistics.notStudied.percentage = terms.length
-    ? Math.round((notStudiedTerms / terms.length) * 100)
-    : 0
-  statistics.learning.count = learningTerms
-  statistics.learning.percentage = terms.length
-    ? Math.round((learningTerms / terms.length) * 100)
-    : 0
-  statistics.mastered.count = masteredTerms
-  statistics.mastered.percentage = terms.length
-    ? Math.round((masteredTerms / terms.length) * 100)
-    : 0
+  if (!terms.length) return statistics
+
+  for (const term of terms) {
+    const status = getTermStatus(term)
+    statistics[status].count++
+  }
+
+  statistics.notStudied.percentage = Math.round(
+    (statistics.notStudied.count / terms.length) * 100,
+  )
+  statistics.learning.percentage = Math.round(
+    (statistics.learning.count / terms.length) * 100,
+  )
+  statistics.mastered.percentage = Math.round(
+    (statistics.mastered.count / terms.length) * 100,
+  )
   return statistics
 }
