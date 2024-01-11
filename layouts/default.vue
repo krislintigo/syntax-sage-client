4bd0f02908cb2276c192f9c93fe37650674096f0
1sses <vladzholon@gmail.com>
2024-01-11 13:26:19 +0300

    working

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,10 +1,9 @@
 <template lang="pug">
-.mx-auto.h-screen(class='max-w-[500px]')
+.mx-auto.h-dvh(class='max-w-[500px]')
   el-header(height='50')
     el-row.pt-6(justify='space-between', align='middle')
       h1.text-3xl(@click='reload') Syntax Sage
-  el-row(justify='center')
-    el-divider(class='!w-11/12 !mb-3')
+    el-divider(class='!mb-3')
   el-main(class='h-[88%]')
     slot
 </template>
