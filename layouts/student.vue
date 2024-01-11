76afbd1128314b6d7397cd2c914e69ca8e37bb0b
1sses <vladzholon@gmail.com>
2024-01-11 13:18:23 +0300

    working

--- layouts/student.vue
+++ layouts/student.vue
@@ -1,13 +1,12 @@
 <template lang="pug">
-.mx-auto.h-screen(class='max-w-[500px]')
+.mx-auto.h-dvh(class='max-w-[500px]')
   el-header(height='50')
     el-row.pt-6(justify='space-between', align='middle')
       h1.text-3xl(@click='reload') Syntax Sage
       el-avatar(@click='navigateTo("/profile")')
         el-icon
           ElIconUser
-  el-row(justify='center')
-    el-divider(class='!w-11/12 !mb-3')
+    el-divider(class='!mb-3')
   el-main#main(class='h-[88%]')
     .mb-5
       el-input.mb-3(
