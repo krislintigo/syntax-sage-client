92b24e776d92b992b935f904f913d4000c6ab471
1sses <vladzholon@gmail.com>
2024-01-28 19:05:52 +0300

    fixes

--- components/InputKeyboard.vue
+++ components/InputKeyboard.vue
@@ -2,22 +2,21 @@
 .flex.flex-col.gap-y-2.bg-zinc-800.p-1.py-3(class='mx-[-20px]')
   el-row.gap-x-1(v-for='(row, i) in keyboard', :key='i', justify='center')
     template(v-for='(key, j) in row', :key='j')
-      template(v-if='key.action === "remove"')
-        .flex.justify-center.items-center.bg-gray-600.rounded(
+      template(v-if='key.action === "backspace"')
+        .flex.justify-center.items-center.border.border-zinc-700.rounded(
           :style='{ width: letterStyle.width * 1.5 + "px" }',
           @click='input = input.slice(0, -1)'
         )
           Icon(size='25', name='ph:backspace')
-      .flex.justify-center.items-center.bg-gray-600.rounded(
+      .flex.justify-center.items-center.border.border-zinc-700.rounded(
         v-else,
         :style='{ width: letterStyle.width + "px" }',
         class='aspect-[2/3]',
         @click='input += key'
       )
         span.leading-none.font-light(
-          :class='{ "mb-1": marginLettersToFix.includes(key) }',
-          :style='{ fontSize: letterStyle.fontSize + "px" }'
-        ) {{ key }}
+          :style='{ fontSize: letterStyle.fontSize + "px", marginBottom: key.margin + "px" }'
+        ) {{ key.letter }}
 </template>
 
 <script setup lang="ts">
@@ -25,36 +24,57 @@ import { useWindowSize } from '@vueuse/core'
 
 const input = defineModel<string>({ required: true })
 
+// const keyboard = [
+//   ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'å'],
+//   ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ö', 'ä'],
+//   ['z', 'x', 'c', 'v', 'b', 'n', 'm', '-', { action: 'remove' }],
+// ]
+
 const keyboard = [
-  ['q', 'w', 'e', 'r', 't', 'y', 'u', 'i', 'o', 'p', 'å'],
-  ['a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'ö', 'ä'],
-  ['z', 'x', 'c', 'v', 'b', 'n', 'm', '-', { action: 'remove' }],
-]
-const marginLettersToFix = [
-  'q',
-  'w',
-  'e',
-  'r',
-  'y',
-  'u',
-  'o',
-  'p',
-  'a',
-  's',
-  'g',
-  'z',
-  'x',
-  'c',
-  'v',
-  'n',
-  'm',
+  [
+    { letter: 'q', action: 'letter', margin: 4 },
+    { letter: 'w', action: 'letter', margin: 4 },
+    { letter: 'e', action: 'letter', margin: 4 },
+    { letter: 'r', action: 'letter', margin: 4 },
+    { letter: 't', action: 'letter', margin: 4 },
+    { letter: 'y', action: 'letter', margin: 4 },
+    { letter: 'u', action: 'letter', margin: 4 },
+    { letter: 'i', action: 'letter', margin: 4 },
+    { letter: 'o', action: 'letter', margin: 4 },
+    { letter: 'p', action: 'letter', margin: 4 },
+    { letter: 'å', action: 'letter', margin: 4 },
+  ],
+  [
+    { letter: 'a', action: 'letter', margin: 4 },
+    { letter: 's', action: 'letter', margin: 4 },
+    { letter: 'd', action: 'letter', margin: 4 },
+    { letter: 'f', action: 'letter', margin: 4 },
+    { letter: 'g', action: 'letter', margin: 4 },
+    { letter: 'h', action: 'letter', margin: 4 },
+    { letter: 'j', action: 'letter', margin: 4 },
+    { letter: 'k', action: 'letter', margin: 4 },
+    { letter: 'l', action: 'letter', margin: 4 },
+    { letter: 'ö', action: 'letter', margin: 4 },
+    { letter: 'ä', action: 'letter', margin: 4 },
+  ],
+  [
+    { letter: 'z', action: 'letter', margin: 4 },
+    { letter: 'x', action: 'letter', margin: 4 },
+    { letter: 'c', action: 'letter', margin: 4 },
+    { letter: 'v', action: 'letter', margin: 4 },
+    { letter: 'b', action: 'letter', margin: 4 },
+    { letter: 'n', action: 'letter', margin: 4 },
+    { letter: 'm', action: 'letter', margin: 4 },
+    { letter: '-', action: 'letter', margin: 0 },
+    { letter: '', action: 'backspace', margin: 0 },
+  ],
 ]
 
 const { width } = useWindowSize()
 
 const letterStyle = computed(() => ({
   width: Math.round(Math.min(width.value, 500) / 13),
-  fontSize: Math.round(Math.min(width.value, 500) / 18),
+  fontSize: Math.round(Math.min(width.value, 500) / 16),
 }))
 </script>
 
